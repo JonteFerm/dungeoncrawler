b5cb262863c9e8a3e5a51d778eb6e64bfb306648
Jonathan Ferm <jonte_ferm@hotmail.com>
2015-01-22 11:39:22 +0100

    Base

---
+++ header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang='en' class='no-js'>
+<head>
+<meta charset='utf-8' />
+<title><?=isset($title) ? $title : 'Template for testprograms in JavaScript'?></title>
+<link rel="stylesheet/less" type="text/css" href="style/style.less">
+<script src="js/less.min.js"></script>
+<script src="js/modernizr.js"></script>
+</head>
+<body>
+<header>
+<h1>Testmiljö för JavaScript</h1>
+</header>
